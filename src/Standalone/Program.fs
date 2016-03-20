63a6c2b318d5982498a46e7a9534668750d5889b
Isaac Abraham <isaac.abraham@gmail.com>
2016-03-20 19:36:17 +0100

    Cleanup with cluster shutdown.

--- src/Standalone/Program.fs
+++ src/Standalone/Program.fs
@@ -1,7 +1,24 @@
 ﻿open System
 open MBraceHelpers
+open MBrace.Thespian
 
 let getTopFiftyResults = performWordCountFromLocalFile 50
+
+let quit (cluster:ThespianCluster) =
+    Console.Clear()
+    printf "Shutting down..."
+    cluster.KillAllWorkers()
+    cluster.ClearSystemLogs()
+    printfn "All done!"
+    Environment.Exit(0)
+
+let doWordCount path (cluster:ThespianCluster) =
+    printfn ""
+    try
+        let results = getTopFiftyResults path |> cluster.Run
+        results |> Seq.iter (printfn "%A")
+    with ex -> printfn "Something went wrong: %O" ex
+    Console.ReadLine() |> ignore
 
 [<EntryPoint>]
 let main argv =
@@ -16,12 +33,8 @@ let main argv =
         printfn "============================="
         printfn ""
         printf "Please provide the path to a text file to do an analysis on: "
-        let path = Console.ReadLine()
-        try
-            let results = getTopFiftyResults path |> cluster.Run
-            results |> Seq.iter (printfn "%A")
-        with ex -> printfn "Something went wrong: %O" ex
 
-        Console.ReadLine() |> ignore
-
+        match Console.ReadLine() with
+        | "exit" -> cluster |> quit
+        | path -> cluster |> doWordCount path
     0
